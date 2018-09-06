8cee64418ff930cfd1d76d14143e4e7c719bb7cc
Ravi <ravi@slsplap218.local>
2018-09-06 16:45:09 +0530

    validation & testcases added and readme file changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # NPM Package To maintain the priority of any thing
 It is used to update the priority from given data array
+_The package is useful in the case where array of objects one key with unique values as priority and 
+out of that if you change in one of the priority, it should automatically update the rest according to the logic_
 
 _To Use This Package For Earlier Versions (<6.0) Of Your Node.js Then Download <1.0.0 Version_
 
@@ -19,13 +21,26 @@ Install with the node package manager [npm](http://npmjs.org):
 ## How To Use?
 
 ```javascript
-let data = [{'id': 1, 'name': 'ravi', 'prio': 1}, {'id': 2, 'name': 'ravi2', 'prio': 2}, {'id': 3, 'name': 'ravi3', 'prio': 3}, {'id': 4, 'name': 'ravi4', 'prio': 4}, {'id': 5, 'name': 'ravi5', 'prio': 5}]
-    let input = {
-      dataKey: 'prio',
-      dataValue: 4,
-      searchKey: 'name',
-      searchValue: 'ravi'
-    }
-  /* OUTPUT */
-[{"id":2,"name":"ravi2","prio":1},{"id":3,"name":"ravi3","prio":2},{"id":4,"name":"ravi4","prio":3},{"id":1,"name":"ravi","prio":4},{"id":5,"name":"ravi5","prio":5}]
+Lets take an example where top 5 brands car into the competition
+on every hits on the car by viewer it sets to new priority
+/* INPUT */
+let data = [
+  {'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, 
+  {'id': 2, 'brand': 'BMW', 'priority': 2}, 
+  {'id': 3, 'brand': 'Audi', 'priority': 3}, 
+  {'id': 4, 'brand': 'Porsche', 'priority': 4}, 
+  {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}
+]
+let input = {
+  dataKey: 'priority',
+  dataValue: 1,
+  searchKey: 'brand',
+  searchValue: 'Porsche'
+}
+/* OUTPUT */
+[{"id":4,"brand":"Porsche","priority":1},
+{"id":1,"brand":"Mercedes-Benz","priority":2},
+{"id":2,"brand":"BMW","priority":3},
+{"id":3,"brand":"Audi","priority":4},
+{"id":5,"brand":"ravi5","Volkswagen":5,"priority":5}]
 ```
--- test/priority.js
+++ test/priority.js
@@ -3,13 +3,57 @@ let expect = chai.expect
 const priority = require('../index.js').priority
 
 describe('Update Priority from array of data', () => {
-  it('Should return same length array', (done) => {
-    let data = [{'id': 1, 'name': 'ravi', 'prio': 1}, {'id': 2, 'name': 'ravi2', 'prio': 2}, {'id': 3, 'name': 'ravi3', 'prio': 3}, {'id': 4, 'name': 'ravi4', 'prio': 4}, {'id': 5, 'name': 'ravi5', 'prio': 5}]
+  it('1. Should throw error when input does not have dataKey', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}]
     let input = {
-      dataKey: 'prio',
-      dataValue: 4,
-      searchKey: 'name',
-      searchValue: 'ravi'
+      dataKey_new: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Input data is invalid. Require dataKey as string');
+    done()
+  })
+  it('2. Should throw error when input does not have dataValue', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}]
+    let input = {
+      dataKey: 'priority',
+      dataValue_new: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Input data is invalid. Require dataValue as number');
+    done()
+  })
+  it('3. Should throw error when input does not have searchKey', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}]
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey_new: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Input data is invalid. Require searchKey as string');
+    done()
+  })
+  it('4. Should throw error when input does not have searchValue', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}]
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue_new: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Input data is invalid. Require searchValue');
+    done()
+  })
+  it('5. Should return same length array', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'ravi5', 'Volkswagen': 5}]
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
     }
     let output = priority(data, input)
     // console.log('daadadad', JSON.stringify(output))
