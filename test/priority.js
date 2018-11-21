7d3a31347541bc3122597e4ddb1a696c1fcb5617
Ravi <ravi@slsplap218.local>
2018-11-21 10:36:32 +0530

    New testcases added

--- test/priority.js
+++ test/priority.js
@@ -4,7 +4,19 @@ const priority = require('../index').priority
 const priorityInBulk = require('../index').priorityInBulk
 
 describe('Update Priority from array of data', () => {
-  it('1. Should throw error when input does not have dataKey', (done) => {
+  it('1. Should throw error when data is not an array', (done) => {
+    // let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
+    let data = {}
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
+    done()
+  })
+  it('2. Should throw error when input does not have dataKey', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey_new: 'priority',
@@ -15,7 +27,7 @@ describe('Update Priority from array of data', () => {
     expect(()=> priority(data, input)).to.throw('Input data is invalid. Require dataKey as string');
     done()
   })
-  it('2. Should throw error when input does not have dataValue', (done) => {
+  it('3. Should throw error when input does not have dataValue', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey: 'priority',
@@ -26,7 +38,7 @@ describe('Update Priority from array of data', () => {
     expect(()=> priority(data, input)).to.throw('Input data is invalid. Require dataValue as number');
     done()
   })
-  it('3. Should throw error when input does not have searchKey', (done) => {
+  it('4. Should throw error when input does not have searchKey', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey: 'priority',
@@ -37,7 +49,7 @@ describe('Update Priority from array of data', () => {
     expect(()=> priority(data, input)).to.throw('Input data is invalid. Require searchKey as string');
     done()
   })
-  it('4. Should throw error when input does not have searchValue', (done) => {
+  it('5. Should throw error when input does not have searchValue', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey: 'priority',
@@ -48,7 +60,7 @@ describe('Update Priority from array of data', () => {
     expect(()=> priority(data, input)).to.throw('Input data is invalid. Require searchValue');
     done()
   })
-  it('5. Should return same length array', (done) => {
+  it('6. Should return same length array', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey: 'priority',
@@ -61,7 +73,7 @@ describe('Update Priority from array of data', () => {
     expect(output.length).to.equal(data.length)
     done()
   })
-  it('6. Should throw error when priorityInBulk function called without input as an array', (done) => {
+  it('7. Should throw error when priorityInBulk function called without input as an array', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = {
       dataKey: 'priority',
@@ -72,7 +84,7 @@ describe('Update Priority from array of data', () => {
     expect(()=> priorityInBulk(data, input)).to.throw('Invalid input provided.');
     done()
   })
-  it('7. Should return same length array with unique priority', (done) => {
+  it('8. Should return same length array with unique priority', (done) => {
     let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
     let input = [{
       dataKey: 'priority',
@@ -92,6 +104,80 @@ describe('Update Priority from array of data', () => {
     }]
     let output = priorityInBulk(data, input)
     expect(output.length).to.equal(data.length)
+    done()
+  })
+  it('9. Should throw error when data is an empty array', (done) => {
+    // let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
+    let data = []
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
+    done()
+  })
+  it('10. Should throw error when data does not contains the search data', (done) => {
+    // let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}]
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    expect(()=> priority(data, input)).to.throw('Can not find the search data');
+    done()
+  })
+  it('11. Should throw error when input is an empty object', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
+    let input = {}
+    
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
+    done()
+  })
+  it('12. Should throw error when input is null', (done) => {
+    let data = [{'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}, {'id': 2, 'brand': 'BMW', 'priority': 2}, {'id': 3, 'brand': 'Audi', 'priority': 3}, {'id': 4, 'brand': 'Porsche', 'priority': 4}, {'id': 5, 'brand': 'Volkswagen', 'priority': 5}]
+    let input = null
+    
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
+    done()
+  })
+  it('13. Should throw error when data is null', (done) => {
+    let data = null
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
+    done()
+  })
+  it('14. Should throw error when data is an array with empty objects', (done) => {
+    let data = [{}]
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    
+    expect(()=> priority(data, input)).to.throw('Can not find the search data');
+    done()
+  })
+  it('15. Should throw error when data is an object', (done) => {
+    let data = {'id': 1, 'brand': 'Mercedes-Benz', 'priority': 1}
+    let input = {
+      dataKey: 'priority',
+      dataValue: 1,
+      searchKey: 'brand',
+      searchValue: 'Porsche'
+    }
+    
+    expect(()=> priority(data, input)).to.throw('Invalid input provided.');
     done()
   })
 })
